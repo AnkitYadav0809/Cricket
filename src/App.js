a3383b0f8ab2f8b02ae15f0c7c964533c1c55e5f
Ankit Yadav <ankityadav60062@gmail.com>
2024-08-16 13:07:06 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import data from './data.js';
-//import Playersrs from './Component/Players.js';
 import Players from "./Component/Players.js";
-
 const App = () => {
   const [players, setPlayers] = useState(data);
   
-  
-
-  return (
+   return (
     <div className="app">
-      <Players players={players}  />
+           <Players players={players}></Players>  
     </div>
   )
 
